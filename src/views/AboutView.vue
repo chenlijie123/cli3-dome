5d77d8f80a9f8f78975ce53b86cf8f295b47a96e
chenlijie <chen.lijie@hxss.com.cn>
2023-03-28 15:29:46 +0800

    init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      {{ num }}
+    </div>
+    <button type="button" @click="increments"> num ++</button>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+const num = ref(0)
+function increments () {
+  num.value++
+}
+</script>
+<style scoped>
+
+</style>
